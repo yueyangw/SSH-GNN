9df6921327bf819c1b1254fcc68382790450ca49
yueyangw <yueyang_wu@outlook.com>
2023-05-18 17:03:02 +0800

    finish spatial_view

---
+++ lib/util.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+LON1 = 103.497579
+LON2 = 104.600359
+LAT1 = 30.297549
+LAT2 = 31.032446
+
+
+def generalID(lon, lat, column_num=15, row_num=15):
+    # 若在范围外的点，返回-1
+    if lon <= LON1 or lon >= LON2 or lat <= LAT1 or lat >= LAT2:
+        return -1
+    # 把经度范围根据列数切割
+    column = (LON2 - LON1) / column_num
+    # 把纬度范围根据行数切割
+    row = (LAT2 - LAT1) / row_num
+    # 二维矩阵坐标索引转换为一维ID，即： （列坐标区域（向下取整）+ 1） + （行坐标区域 * 列数）
+    return int((lon - LON1) / column) + 1 + int((lat - LAT1) / row) * column_num
+
+
+def get_air_quality_stations():
+    pointdata = pd.read_csv('data/point.csv', encoding='UTF-8')
+    stations = []
+    for i in range(7):
+        stations.append((pointdata.iat[i, 3], pointdata.iat[i, 4]))
+    return stations
+
+
+def get_latlon_by_id(id):
+    column = (LON2 - LON1) / 15
+    row = (LAT2 - LAT1) / 15
+    x = id // 15
+    y = id % 15
+    return LAT1 + row * x + row / 2, LON1 + column * y + column / 2
+
+
+def get_id_by_idx(x, y):
+    return x * 15 + y
+
+
+if __name__ == '__main__':
+    get_air_quality_stations()
--- model/approximation/spatial_view.py
+++ model/approximation/spatial_view.py
@@ -1,19 +1,56 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from lib.util import *
+import math
+
+
+def dis(x, y, xx, yy):
+    return math.sqrt((x - xx) ** 2 + (y - yy) ** 2)
+
 
 class SpatialModel(nn.Module):
+    '''
+    stations: [(lat1, lon1), (lat2, lon2), ...]
+    Input stations list at initialize.
+    '''
 
-    def __init__(self):
+    def __init__(self, stations):
         super(SpatialModel, self).__init__()
-        W = torch.randn((6, 1), requires_grad=True)
+        W = torch.randn(7, requires_grad=True)
         self.W = nn.Parameter(W)
+        self.d = [[[0 for _ in range(7)] for _ in range(15)] for _ in range(15)]
+        dists = []
+        self.stations = []
+        for i in range(7):
+            self.stations.append(generalID(stations[i][0], stations[i][1]))
+        for i in range(15):
+            for j in range(15):
+                for k in range(7):
+                    lon, lat = stations[k][0], stations[k][1]
+                    p_id = get_id_by_idx(i, j)
+                    p_lat, p_lon = get_latlon_by_id(p_id)
+                    dist = dis(p_lat, p_lon, lat, lon)
+                    dists.append(dist)
+                    self.d[i][j][k] = dist
+        self.delta = np.std(dists)
 
-    def forward(self, x, near_s):
-        pass
+    def proximity(self, posx, posy, station):
+        return math.exp(-self.d[posx][posy][station] / (self.delta ** 2))
+
+    def forward(self, x: torch.Tensor):
+        res = torch.zeros(x.shape)
+        for i in range(15):
+            for j in range(15):
+                for k in range(7):
+                    stationx, stationy = self.stations[k] // 15, self.stations[k] % 15
+                    res[i, j] += self.proximity(i, j, k) * torch.mul(self.W, x[stationx, stationy])
+        return res
 
 
 if __name__ == '__main__':
-    model = SpatialModel()
-    for k, v in model.named_parameters():
-        print(k, v)
+    model = SpatialModel(get_air_quality_stations())
+    x = np.load("data/air_quality.npy")
+    x = torch.from_numpy(x)
+    y = model(x[0])
+    print(y)
