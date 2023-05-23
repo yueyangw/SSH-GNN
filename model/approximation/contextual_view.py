484055eb87fcee99325bd23f87b29423c3c2ac0e
fengxingyue <2717134711@qq.com>
2023-05-23 17:31:41 +0800

    contextv2

--- model/approximation/contextual_view.py
+++ model/approximation/contextual_view.py
@@ -12,41 +12,43 @@ def similarity(xci, xcj):
 
 class ContextualViewModel(nn.Module):
 
-    def __init__(self, stations: list, size=(15, 10), context_length=6):
+    def __init__(self, stations: list, size=(15, 10, 6), feature_length=7):
         super(ContextualViewModel, self).__init__()
         self.size = size
-        self.context_length = context_length
-        W = torch.randn((context_length, context_length), dtype=torch.float64, requires_grad=True)
+        size1 = torch.randn(size)   # context feature
+        self.feature_length = feature_length
+        W = torch.randn((feature_length, feature_length), dtype=torch.float64, requires_grad=True)
         self.W = nn.Parameter(W)
-        self.d = [[[0 for _ in range(context_length)] for _ in range(size[1])] for _ in range(size[0])]
-        dists = []
+        self.d = [[[0 for _ in range(feature_length)] for _ in range(size[1])] for _ in range(size[0])]
         self.stations = []
-        for i in range(context_length):
-            self.stations.append(generalID(stations[i][0]))
+        res = []
+        for i in range(feature_length):
+            self.stations.append(generalID(stations[i][0], stations[i][1]),)
         for i in range(size[0]):
             for j in range(size[1]):
-                for k in range(context_length):
-                    xi = stations[k][0]
-                    p_id = get_id_by_idx(i, j)
-                    xj = get_latlon_by_id(p_id)
-                    sim = similarity(xi, xj)
-                    sim.append(sim)
-                    self.d[i][j][k] = sim
-        self.delta = np.std(dists)
+                for k in range(feature_length):
+                    xi,xj = stations[k][0], stations[k][1]
+                    for f in range(size[2]):
+                        ri = size1[xi][xj][f]
+                        rj = size1[i][j][f]
+                        sim = similarity(ri, rj)
+                        res.append(float(sim))
+                    self.d[i][j][k] = res
 
     def forward(self, x: torch.Tensor):
         res = torch.zeros(x.shape)
         for i in range(self.size[0]):
             for j in range(self.size[1]):
-                for k in range(self.context_length):
+                for k in range(self.feature_length):
                     stationx, stationy = self.stations[k] // self.size[0], self.stations[k] % self.size[1]
-                    res[i, j] += torch.matmul(x[stationx, stationy], self.W)
+                    res[i, j] += self.d[i][j][k] * torch.matmul(x[stationx, stationy], self.W)
         return res
 
 
 if __name__ == '__main__':
-    model = ContextualViewModel(get_air_quality_stations(), (15, 10))
-    x = np.load("data/*****")
+
+    model = ContextualViewModel(torch.randint(1,9,(7,2)), (15, 10, 6))
+    x = np.load("data/air_quality.npy")
     x = torch.from_numpy(x)
     y = model(x[0])
     print(y)
